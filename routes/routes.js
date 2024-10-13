849f053663474bcedf7aece4723d611601c6ae6e
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-10-13 23:34:58 +0300

    done

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const controller = require("../controllers/controller");
 
 router.get("/contacts", controller.get);
 router.post("/contacts", controller.create);
+router.patch("/contacts/:contactId", controller.update);
 router.put("/contacts/:contactId", controller.update);
 router.delete("/contacts/:contactId", controller.remove);
 
